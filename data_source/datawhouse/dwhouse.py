11cd473131c8dd5e0b4c1009f193d33110562507
Lucas_Policano <lucas1336policano@gmail.com>
2023-07-05 21:17:36 -0300

    datawerehouse

--- data_source/datawhouse/dwhouse.py
+++ data_source/datawhouse/dwhouse.py
@@ -12,16 +12,17 @@ load_dotenv()
 
 db_host = os.getenv("DB_HOST")
 db_port = os.getenv("DB_PORT")
-db_database = os.getenv("DB_NAME")
+db_database = os.getenv("DB_DATABASE")
 db_user = os.getenv("DB_USER")
 db_password = os.getenv("DB_PASSWORD")
 
 conn = psycopg2.connect(host=db_host, port=db_port, dbname=db_database, user=db_user, password=db_password)
+cursor = conn.cursor()
 
-brasil_csv = pd.read_csv('../data_source/curated/brazil_data/historico_covidbr_todo_periodo_regiao_brasil.csv')
-brasil_estado_csv = pd.read_csv('../data_source/curated/brazil_data/historico_covidbr_todo_periodo_regiao_estados.csv')
-brasil_vacinacao = pd.read_csv('../data_source/curated/brazil_data/clean_data_mortos_apos_vacinacao_brasil_correlacao.csv')
-new_zeland_csv = pd.read_csv('../data_source/curated/new_zeland_data/clean_data_obitos_totais_nz_covid.csv')
+#brasil_csv = pd.read_csv('../data_source/curated/brazil_data/historico_covidbr_todo_periodo_regiao_brasil.csv')
+#brasil_estado_csv = pd.read_csv('../data_source/curated/brazil_data/historico_covidbr_todo_periodo_regiao_estados.csv')
+#brasil_vacinacao = pd.read_csv('../data_source/curated/brazil_data/clean_data_mortos_apos_vacinacao_brasil_correlacao.csv')
+#new_zeland_csv = pd.read_csv('../data_source/curated/new_zeland_data/clean_data_obitos_totais_nz_covid.csv')
 
 tabelas =  """CREATE TABLE IF NOT EXISTS public.brasil (
     id uuid NOT NULL CONSTRAINT br_pk PRIMARY KEY CONSTRAINT br_pk_gen DEFAULT gen_random_uuid(),
@@ -68,12 +69,13 @@ tabelas =  """CREATE TABLE IF NOT EXISTS public.brasil (
     casosnovos integer NOT NULL,
     casosacumulado integer NOT NULL,
     obitosnovos integer NOT NULL,
-    obitosacumulado);"""
+    obitosacumulado integer NOT NULL);
+    """
 
-def create_table(engine, table_name, columns):
-    columns_query = ', '.join(f'"{col}" VARCHAR' for col in columns)
-    query = f"CREATE TABLE IF NOT EXISTS {table_name} ({columns_query})"
-    cursor = engine.connect().execution_options(isolation_level="AUTOCOMMIT").execute(text(query))
-
-query = 'SELECT * FROM Brasil'
-df = pd.read_sql_query(query, engine)
+if tabelas:
+    try:
+        cursor.execute(tabelas)
+        conn.commit()
+    except psycopg2.Error as e:
+        conn.rollback()
+        print(f"Erro: {str(e)}")
